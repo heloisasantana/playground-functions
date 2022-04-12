93c442146d3880a61758ac4d1adcda36e937d1a1
heloisasantana <heloisasantana.silva@hotmail.com>
2022-04-12 15:22:54 -0300

    Conclusão do Requisito 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,10 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
-}
+function compareTrue(amostra1, amostra2) {
+  if (amostra1 === true && amostra2 === true) {
+  return true
+  }
+  else {return false}
+  }
 
 // Desafio 2
 function calcArea() {
